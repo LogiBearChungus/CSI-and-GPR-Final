b0e212ff4c504fad09bbb79e8d2cf06e87b0b019
Logan McCandless <loganmccandless8@gmail.com>
2025-11-17 19:34:42 -0500

    Worked on the Voxel Mesh Generator
    cleaned up some of the unity stuff. still need to write the face creation functions.

---
+++ CSI and GPR Final/Assets/Scripts/VoxelMeshGenerator.cs
@@ -0,0 +1,104 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoxelMeshGenerator : MonoBehaviour
+{
+
+    public Material voxelMaterial;
+    public int[,,] voxelData;
+    [SerializeField] int chunkSize = 16;
+    const int width = 1, height = 1;
+
+    private Mesh mesh;
+    private List<Vector3> vertices = new List<Vector3>();
+    private List<int> triangles = new List<int>();
+    private List<Vector2> uvs = new List<Vector2>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Generate Voxel Data
+        voxelData = new int[chunkSize, chunkSize, chunkSize];
+        for (int x = 0; x < chunkSize; x++)
+        {
+            for (int y = 0; y < chunkSize; y++)
+            {
+                for (int z = 0; z < chunkSize; z++)
+                {
+                    if ((x + y + z) % 2 == 0)
+                    {
+                        voxelData[x, y, z] = 1;
+                    }
+                    else
+                    {
+                        voxelData[x, y, z] = 0;
+
+                    }
+                }
+            }
+
+
+        }
+        GenerateMesh();
+    }
+
+    void GenerateMesh()
+    {
+        vertices.Clear();
+        triangles.Clear();
+        uvs.Clear();
+
+        // Loop through all voxels
+        for (int x = 0; x < chunkSize; x++)
+        {
+            for (int y = 0; y < chunkSize; y++)
+            {
+                for (int z = 0; z < chunkSize; z++)
+                {
+                    if (voxelData[x, y, z] == 1)
+                    {
+                        CreateVoxel(x, y, z);
+                    }
+                }
+            }
+        }
+
+        // Create or update the mesh
+        if (mesh == null)
+        {
+            mesh = new Mesh();
+            GetComponent<MeshFilter>().mesh = mesh;
+        }
+        mesh.Clear();
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.uv = uvs.ToArray();
+        mesh.RecalculateNormals();
+
+        // Apply material
+        if (voxelMaterial != null)
+        {
+            GetComponent<MeshRenderer>().material = voxelMaterial;
+        }
+    }
+        void createVoxel(int x, int y, int z)
+        {
+            // Check each face and only create it if it's exposed
+            if (!IsVoxelSolid(x, y + 1, z)) CreateTopFace(x, y, z);      // Top
+            if (!IsVoxelSolid(x, y - 1, z)) CreateBottomFace(x, y, z);   // Bottom
+            if (!IsVoxelSolid(x - 1, y, z)) CreateLeftFace(x, y, z);     // Left
+            if (!IsVoxelSolid(x + 1, y, z)) CreateRightFace(x, y, z);    // Right
+            if (!IsVoxelSolid(x, y, z + 1)) CreateFrontFace(x, y, z);    // Front
+            if (!IsVoxelSolid(x, y, z - 1)) CreateBackFace(x, y, z);     // Back
+
+        }
+        bool IsVoxelSolid(int x, int y, int z)
+        {
+            // Check if position is out of bounds
+            if (x < 0 || x >= chunkSize || y < 0 || y >= chunkSize || z < 0 || z >= chunkSize)
+            {
+                return false; // Treat out of bounds as air
+            }
+            return voxelData[x, y, z] == 1;
+        }
+}
